e9909ee5e924526d52ea003e41539f2b68ca4dcb
aluno <anapchavesweb@gmail.com>
2020-02-11 21:55:24 -0400

    xxx

--- app/src/main/java/com/example/myapplication3710/MainActivity.java
+++ app/src/main/java/com/example/myapplication3710/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.myapplication3710;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv1;
+    Button botao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = findViewById(R.id.textView);
+        botao=findViewById(R.id.button2);
 
+      //  botao.setOnClickListener();
         //TODO fazer tela login
+    }
+
+    public void funcao1(){
+        tv1.setText("Android");
     }
 }
